620bccbb798bc42bafc89cc55ab0b175c8d91eae
Manoel Almeida <manoel.almeida@sptech.school>
2023-01-19 22:24:06 -0300

    To-do application bootstrap

---
+++ to-do-oauth2-resource-server/src/main/java/school/sptech/todooauth2resourceserver/ToDoOauth2ResourceServerApplication.java
@@ -0,0 +1,13 @@
+package school.sptech.todooauth2resourceserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoOauth2ResourceServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToDoOauth2ResourceServerApplication.class, args);
+	}
+
+}
---
+++ to-do-oauth2-resource-server/src/test/java/school/sptech/todooauth2resourceserver/ToDoOauth2ResourceServerApplicationTests.java
@@ -0,0 +1,13 @@
+package school.sptech.todooauth2resourceserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoOauth2ResourceServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
